002b3a0d3d867216b4d06fef5c5491b4a838ac2d
Zhen Qian <zqian@umich.edu>
2018-07-02 00:15:09 -0400

    added template for index.html

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,31 @@
 from flask import Flask
-application = Flask(__name__)
 
-@application.route("/")
-def hello():
-    return "Hello World!"
+from flask import render_template
+from flask import request
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('index.html', course_id=None)
+
+@app.route("/course_id/<int:course_id>")
+def index_with_param(course_id=None):
+    return render_template('index.html', course_id=course_id)
+
+
+@app.route("/files")
+def files(name=None):
+    return render_template('files.html', name=name)
+
+
+@app.route("/grades")
+def grades():
+    return render_template('grades.html')
+
+@app.route("/files/multiple_bar")
+def multiple_bar():
+    return render_template('small_multiples_files_bar_chart.html')
 
 if __name__ == "__main__":
-    application.run()
+    app.run()
