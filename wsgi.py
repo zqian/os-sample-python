2d4325b4b4b1b2e3304b1093df19458f87c139c0
Zhen Qian <zqian@umich.edu>
2018-07-02 00:24:58 -0400

    change app to application

--- wsgi.py
+++ wsgi.py
@@ -3,29 +3,29 @@ from flask import Flask
 from flask import render_template
 from flask import request
 
-app = Flask(__name__)
+application= Flask(__name__)
 
-@app.route("/")
+@application.route("/")
 def index():
     return render_template('index.html', course_id=None)
 
-@app.route("/course_id/<int:course_id>")
+@application.route("/course_id/<int:course_id>")
 def index_with_param(course_id=None):
     return render_template('index.html', course_id=course_id)
 
 
-@app.route("/files")
+@application.route("/files")
 def files(name=None):
     return render_template('files.html', name=name)
 
 
-@app.route("/grades")
+@application.route("/grades")
 def grades():
     return render_template('grades.html')
 
-@app.route("/files/multiple_bar")
+@application.route("/files/multiple_bar")
 def multiple_bar():
     return render_template('small_multiples_files_bar_chart.html')
 
 if __name__ == "__main__":
-    app.run()
+    application.run()
